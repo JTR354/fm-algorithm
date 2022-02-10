ba18d47b7b4d79457eafa84bc07417f3680dccbf
jtr354 <jtr354@foxmail.com>
2022-02-10 23:09:36 +0800

    feat:[remove linked list elements]

--- src/linked-list/remove-linked-list-elements/index.js
+++ src/linked-list/remove-linked-list-elements/index.js
@@ -4,16 +4,26 @@ exports.removeElements = function removeElements(head, val) {
   }
   head.next = removeElements(head.next, val);
   if (head.val === val) {
+    const next = head.next;
+    delete head.next;
     delete head.val;
-    return head.next;
+    return next;
   }
   return head;
 };
 
-// exports.removeElements2 = (head, val) => {
-//   if (head == null) {
-//     return head;
-//   }
-//   const ele = { next: head };
-//   let p;
-// };
+exports.removeElements2 = (head, val) => {
+  const ele = { next: head };
+  let p = ele;
+  while (p.next) {
+    if (p.next.val === val) {
+      const next = p.next.next;
+      delete p.next.val;
+      delete p.next.next;
+      p.next = next;
+      continue;
+    }
+    p = p.next;
+  }
+  return ele.next;
+};
--- src/linked-list/remove-linked-list-elements/index.spec.js
+++ src/linked-list/remove-linked-list-elements/index.spec.js
@@ -1,27 +1,41 @@
 const LinkNodeList = require('../link-node-list');
-const { removeElements } = require('.');
+const { removeElements, removeElements2 } = require('.');
 describe('linked-list', () => {
   describe('remove-linked-list-elements', () => {
     test('203-1', () => {
       const head = [1, 2, 6, 3, 4, 5, 6],
-        val = 6;
+        val = 6,
+        result = [1, 2, 3, 4, 5];
       const nodeList = new LinkNodeList(head);
       removeElements(nodeList.head, val);
-      expect(nodeList.toString()).toEqual([1, 2, 3, 4, 5]);
+      expect(nodeList.toString()).toEqual(result);
+      const nodeList2 = new LinkNodeList(head);
+      removeElements2(nodeList2.head, val);
+      expect(nodeList2.toString()).toEqual(result);
     });
     test('203-2', () => {
       const head = [],
-        val = 1;
+        val = 1,
+        result = [];
       const nodeList = new LinkNodeList(head);
       removeElements(nodeList.head, val);
-      expect(nodeList.toString()).toEqual([]);
+      expect(nodeList.toString()).toEqual(result);
+
+      const nodeList2 = new LinkNodeList(head);
+      removeElements2(nodeList2.head, val);
+      expect(nodeList2.toString()).toEqual(result);
     });
     test('203-3', () => {
       const head = [7, 7, 7, 7],
-        val = 7;
+        val = 7,
+        result = [];
       const nodeList = new LinkNodeList(head);
       removeElements(nodeList.head, val);
-      expect(nodeList.toString()).toEqual([]);
+      expect(nodeList.toString()).toEqual(result);
+
+      const nodeList2 = new LinkNodeList(head);
+      removeElements2(nodeList2.head, val);
+      expect(nodeList2.toString()).toEqual(result);
     });
   });
 });
